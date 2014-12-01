f223892a35637e846b7feb4e5f06eb20b8e4721a
Chuck Pinkert <chuck.pinkert@sqor.com>
2014-12-01 10:26:09 -0800

    initial commit

---
+++ FPSBarBinding/ApiDefinition.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MonoTouch.ObjCRuntime;
+using MonoTouch.UIKit;
+using MonoTouch.Foundation;
+
+namespace FPSBarBinding
+{
+    [BaseType (typeof (UIWindow))]
+    interface FPSBar 
+    {
+        [Export ("showsAverage")]
+        bool ShowsAverage { get; set; }
+
+        [Export ("desiredChartUpdateInterval")]
+        double DesiredChartUpdateInterval { get; set; }
+
+        [Static, Export ("sharedInstance")]
+        FPSBar SharedInstance { get; }
+
+    } 
+}
+
